4f06b5025f25a71156f56fb6ab00d3151d48c545
MortenJuul <54556492+MortenJuul@users.noreply.github.com>
2021-10-21 21:47:55 -0700

    login complete

--- public/js/login.js
+++ public/js/login.js
@@ -1,32 +1,48 @@
 // Get the modal
 var container = document.getElementById("loginBox");
+var loginContent = document.getElementById("loginContent");
+var profileContent = document.getElementById("profileContent");
+var profileMenu = document.getElementById("profileMenu");
 
 // Get the button that opens the modal
 var login = document.getElementById("loginBtn");
 var register = document.getElementById("registerBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var profile = document.getElementById("profileBtn");
+var profileLink = document.getElementById("profileLink");
+var logoutLink = document.getElementById("logoutLink");
+var close1 = document.getElementById("close1");
+var close2 = document.getElementById("close2");
 
 // When the user clicks on the button, open the modal
 login.onclick = function() {
   container.style.display = "block";
+  loginContent.style.display = "block";
   document.getElementById("loginTab").click();
 }
 registerBtn.onclick = function() {
-    container.style.display = "block";
-    document.getElementById("registerTab").click();
-  }
+  container.style.display = "block";
+  loginContent.style.display = "block";
+  document.getElementById("registerTab").click();
+}
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+close1.onclick = function() {
   container.style.display = "none";
+  loginContent.style.display = "none";
+  profileContent.style.display = "none";
+}
+close2.onclick = function() {
+  container.style.display = "none";
+  loginContent.style.display = "none";
+  profileContent.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == container) {
     container.style.display = "none";
+    loginContent.style.display = "none";
+    profileContent.style.display = "none"; 
   }
 }
 
@@ -50,4 +66,193 @@ function openTab(evt, tabName) {
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
+  };
+
+// Registration form function
+const regForm = document.getElementById('reg-form')
+regForm.addEventListener('submit', registerUser)
+
+async function registerUser(event) {
+  event.preventDefault()
+  const username = document.getElementById('r-username').value
+  const password = document.getElementById('r-password').value
+  const firstname = document.getElementById('r-firstname').value
+  const lastname = document.getElementById('r-lastname').value
+  const email = document.getElementById('r-email').value
+
+
+  const result = await fetch('/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      username,
+      password,
+      firstname,
+      lastname,
+      email
+    })
+  }).then((res) => res.json())
+
+  if (result.status === 'ok') {
+    // everything went fine
+    alert('You have now been signed up. Please log in.')
+    document.getElementById("loginBtn").click();
+  } else {
+    localStorage.clear();
+    alert(result.error)
   }
+}
+
+// Login form function
+const loginForm = document.getElementById('login-form')
+loginForm.addEventListener('submit', loginUser)
+
+async function loginUser(event) {
+  event.preventDefault()
+  const username = document.getElementById('username').value
+  const password = document.getElementById('password').value
+
+  const result = await fetch('/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      username,
+      password
+    })
+  }).then((res) => res.json())
+
+  if (result.status === 'ok') {
+    // everything went fine
+    // console.log('Got the token: ', parseJwt(result.data))
+    localStorage.setItem('token', result.data)
+    // alert('Success')
+    
+    // Close login form
+    container.style.display = "none";
+    loginContent.style.display = "none";
+    profileContent.style.display = "none";
+
+    // switch login/profile UI
+    loginBtn.style.display = "none";
+    registerBtn.style.display = "none";
+    profileMenu.style.display = "block"; 
+
+    document.getElementById("profileBtn").innerText = parseJwt(result.data).firstname + "...";
+
+  } else {
+    const errorMessage = document.getElementById("login-error-message");
+    localStorage.clear();
+    errorMessage.innerText = result.error;
+    errorMessage.style.display = 'block';
+  }
+}
+
+// Get profile page
+
+profile.addEventListener('click', getUser);
+profileLink.addEventListener('click', getUser);
+
+async function getUser(event) {
+  event.preventDefault()
+
+  const result = await fetch('/profile', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      token: localStorage.getItem('token')
+    })
+  }).then((res) => res.json())
+
+  if (result.status === 'ok') {
+
+    container.style.display = "block"
+    profileContent.style.display = "block"
+
+    let profileInfo = parseJwt(localStorage.getItem('token'));
+    document.getElementById("p-firstname").value = profileInfo.firstname
+    document.getElementById("p-lastname").value = profileInfo.lastname
+    document.getElementById("p-email").value = profileInfo.email
+    document.getElementById("p-username").value = profileInfo.username
+
+  } else {
+    alert(result.error)
+    localStorage.clear();
+    window.location.reload(false);
+  }
+}
+
+// Logout
+logoutLink.addEventListener('click', logout);
+
+async function logout(event) {
+  event.preventDefault()
+
+  const result = await fetch('/logout', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then((res) => res.json())
+
+  if (result.status === 'ok') {
+    // everythign went fine
+    // alert('Success')
+    alert("Successfully logged out");
+    localStorage.clear();
+    window.location.reload(false);
+
+  } else {
+    alert(result.error);
+    localStorage.clear();
+    window.location.reload(false);
+  }
+}
+
+// Get time of day greeting
+const timeOfDay = () => {
+  const date = new Date;
+  let hours = date.getHours(); 
+  return (hours < 12)? "Morning" : ((hours <= 18 && hours >= 12 ) ? "Afternoon" : "Night");
+};
+
+// Decode JWT token
+const parseJwt = (token) => {
+  try {
+    return JSON.parse(atob(token.split('.')[1]));
+  } catch (e) {
+    return null;
+  }
+};
+
+// Get user
+try {
+  let greetName = parseJwt(localStorage.getItem('token')).firstname;
+
+  // Insert welcome message
+  document.getElementById("welcome-message").innerText = `Good ${timeOfDay()}, ${greetName}. Welcome back!`;
+  document.getElementById("welcome-container").style.display = "block";
+
+  // switch login/profile UI
+  loginBtn.style.display = "none";
+  registerBtn.style.display = "none";
+  profileMenu.style.display = "block"; 
+
+  // check if session has expired
+  if(Math.round(Date.now()/1000) >= parseInt(parseJwt(localStorage.getItem('token')).exp)){
+    alert("Your session has expired. Please log in again to continue");
+    localStorage.clear();
+    window.location.reload(false);
+  }
+
+} catch (e) {
+  // switch login/profile UI
+  loginBtn.style.display = "block";
+  registerBtn.style.display = "block";
+  profileMenu.style.display = "none"; 
+}
--- routes/router.js
+++ routes/router.js
@@ -1,12 +1,163 @@
 const express = require("express");
+const session = require('express-session')
 const mongoose = require("mongoose");
 const path = require("path");
 const auth = require("http-auth");
+const bodyParser = require('body-parser')
+const passport = require('passport');
 const { check, validationResult } = require("express-validator");
+const Users = require("../models/Users");
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const JWT_SECRET = process.env.JWT_SECRET
 
 const router = express.Router();
 const Contact = mongoose.model("Contact");
+router.use(bodyParser.json())
 
+router.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+}))
+
+// Check token expiry
+router.use(async (req, res, next) => {
+  if(req.session.token){
+    const token = req.session.token
+    try {
+      await jwt.verify(token, JWT_SECRET)
+    } catch (err) {
+      alert("Session expired. Please log in")
+      res.json({ status: 'error', error: 'Session expired' })
+      // res.redirect('/')
+    }
+  }
+  next()
+});
+
+// Log out
+router.post('/logout', async (req, res) => {
+  try {
+		res.json({ status: 'ok' })
+	} catch (error) {
+		res.json({ status: 'error', error: 'Failed to log out' });
+	}
+});
+
+// View profile
+router.post('/profile', async (req, res) => {
+  const { token } = req.body
+  try {
+    jwt.verify(token, JWT_SECRET)		
+		res.json({ status: 'ok' })
+	} catch (error) {
+		res.json({ status: 'error', error: 'Session expired' });
+	}
+})
+
+// Change password -> Not used but available
+router.post('/change-password', async (req, res) => {
+	const { token, newpassword: plainTextPassword } = req.body
+
+	if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid password' })
+	}
+
+	if (plainTextPassword.length < 5) {
+		return res.json({
+			status: 'error',
+			error: 'Password too small. Should be atleast 6 characters'
+		})
+	}
+
+	try {
+		const user = jwt.verify(token, JWT_SECRET)
+		const _id = user.id
+		const password = await bcrypt.hash(plainTextPassword, 10)
+
+		await Users.updateOne(
+			{ _id },
+			{
+				$set: { password }
+			}
+		)
+		res.json({ status: 'ok' })
+	} catch (error) {
+		res.json({ status: 'error', error: ';))' })
+	}
+})
+
+// Login
+router.post('/login', async (req, res) => {
+	const { username, password } = req.body
+	const user = await Users.findOne({ username }).lean()
+
+	if (!user) {
+		return res.json({ status: 'error', error: 'Invalid username/password' })
+	}
+
+	if (await bcrypt.compare(password, user.password)) {
+		// the username, password combination is successful
+		const token = jwt.sign(
+			{
+				id: user._id,
+				username: user.username,
+        firstname: user.firstname,
+        lastname: user.lastname,
+        email: user.email
+			},
+			JWT_SECRET,
+      {expiresIn: '60s'}
+		);
+    req.session.token = token;
+		return res.json({ status: 'ok', data: token })
+	}
+
+	res.json({ status: 'error', error: 'Invalid username/password' })
+})
+
+// Register user
+router.post('/register', async (req, res) => {
+	const { username, password: plainTextPassword, firstname, lastname, email } = req.body
+
+	if (!username || typeof username !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid username' })
+	}
+
+	if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid password' })
+	}
+
+	if (plainTextPassword.length < 5) {
+		return res.json({
+			status: 'error',
+			error: 'Password too short. Should be at least 6 characters'
+		})
+	}
+
+	const password = await bcrypt.hash(plainTextPassword, 10)
+
+	try {
+		const response = await Users.create({
+			username,
+			password,
+      firstname,
+      lastname,
+      email
+		})
+		console.log('User created successfully: ', response)
+	} catch (error) {
+		if (error.code === 11000) {
+			// duplicate key
+			return res.json({ status: 'error', error: 'Username already in use' })
+		}
+		throw error
+	}
+	res.json({ status: 'ok' })
+});
+
+// Routes
 router.get("/", (req, res) => {
   // res.send("it works!");
   res.sendFile(path.join(__dirname, "../public/views/", "index.html"));
@@ -17,7 +168,6 @@ router.get("/contact", (req, res) => {
 });
 
 // add validation here
-
 router.post(
   "/form",
   [
@@ -55,7 +205,6 @@ router.post(
           res.sendFile(path.join(__dirname, "../public/views/", "index.html"));
         })
         .catch((err) => {
-          console.log(err);
           res.send("Sorry! Something went wrong.");
         });
     } else {
