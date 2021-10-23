8858d881efbdf8451a65d8ecdf9e6b7cefe8b2e2
Michelle Naim <michellenaim@gmail.com>
2021-10-23 14:46:15 -0700

    Fix smooth scroll to top

--- public/js/top.js
+++ public/js/top.js
@@ -2,7 +2,7 @@
 var mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = () => {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -14,6 +14,12 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  document.body.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+  document.documentElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
 }
